1887bc0abe4394ecc7a44205a21e87704b35fe84
Jonathan-AI <76454930+Jonathan-AI@users.noreply.github.com>
2024-07-27 21:16:04 +0200

    Add files via upload

---
+++ function_test.R
@@ -0,0 +1,13 @@
+#' Title of your function
+#'
+#' Description of your function
+#' 
+#' @param x A numeric value
+#' @return The square of x
+#' @export
+#'
+#' @examples
+#' yourfunction(2)
+yourfunction <- function(x) {
+  return(x^2)
+}
